cd90ed304fa90cec68c2552476942edb8bcfce34
Bartek-str <bartlomiejstruzyk@gmail.com>
2019-10-07 20:24:30 +0200

    Dokończona karuzela wraz z funkcjonalnością

--- development/js/index.js
+++ development/js/index.js
@@ -1,35 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
+
    var arrowLeft = document.getElementById('arrowLeft');
-   console.log(arrowLeft);
    var arrowRight = document.getElementById('arrowRight');
-   console.log(arrowRight);
    var slides = document.querySelectorAll('.carousel .slide');
-   console.log(slides);
    var idSlide = 0;
 
    slides[idSlide].style.display = 'block';
-
 
    arrowLeft.addEventListener('click', prevP);
    arrowRight.addEventListener('click', nextP);
 
    function prevP(event) {
-       slides[idSlide].style.display = 'none';
-       idSlide--;
-       if (idSlide < 0) {
-           idSlide = slides.length - 1;
+       slides[idSlide].style.transitionDuration = '1.5s';
+       slides[idSlide].style.transform = 'translateX(300px)';
+       slides[idSlide].style.opacity = '0';
+
+       var time = setTimeout(function () {
+           slides[idSlide].style.display = 'none';
+           slides[idSlide].style.transform = '';
+           slides[idSlide].style.opacity = '';
+           idSlide--;
+           if (idSlide < 0) {
+               idSlide = slides.length - 1;
+               slides[idSlide].style.display = 'block';
+           }
            slides[idSlide].style.display = 'block';
-       }
-       slides[idSlide].style.display = 'block';
+       }, 1500);
    }
 
    function nextP(event) {
-       slides[idSlide].style.display = 'none';
-       idSlide++;
-       if (idSlide >= slides.length) {
-           idSlide = 0;
+       slides[idSlide].style.transitionDuration = '1.5s';
+       slides[idSlide].style.transform = 'translateX(-300px)';
+       slides[idSlide].style.opacity = '0';
+
+       var time = setTimeout(function () {
+           slides[idSlide].style.display = 'none';
+           slides[idSlide].style.transform = '';
+           slides[idSlide].style.opacity = '';
+           idSlide++;
+           if (idSlide >= slides.length) {
+               idSlide = 0;
+               slides[idSlide].style.display = 'block';
+           }
            slides[idSlide].style.display = 'block';
-       }
-       else slides[idSlide].style.display = 'block';
+       }, 1500)
    }
 });
