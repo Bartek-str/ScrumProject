6cab5491f76fe1196f292a5c27dd27f734176cbd
Franc91 <franczakszymon@gmail.com>
2019-10-12 00:09:25 +0200

    stworzono dodawnie przepisu wraz z zastosowaniem local storage do poprawy przyciski dodawnia

--- development/js/app.js
+++ development/js/app.js
@@ -99,15 +99,91 @@ document.addEventListener('DOMContentLoaded', function () {
     function addRecipes(event){
         screen.style.display = "none";
         addRecipesPage.style.display = "block";
+    };
+
+    //dodawanie kroków
+    var addStepBtn = document.querySelector("#step-add-btn")
+    var step = document.querySelector(".instruction");
+    var addIngridientBtn = document.querySelector("#ingridient-add-btn");
+    var ingridient = document.querySelector('.ingridients');
+
+    //tworzenie nowego li do list 
+
+    function createNewIngridient(ingridient){
+        var newIngridient = document.createElement("li");
+        newIngridient.innerText=ingridient;
+        listOfIngridients.appendChild(newIngridient);
     }
 
+    function createNewSteps(step){
+        var newStep = document.createElement("li");
+        newStep.innerText = step;
+        listOfStepsRecipe.appendChild(newStep);
+
+    }
+//funkcja dodawania nowych li po kliknieciu w przycisk
+    function addNewIngridient(event){
+        event.preventDefault();
+        newRecipe.ingridients.push(ingridient.value);
+        createNewIngridient(ingridient.value);
+
+    }
+    function addNewStep(event){
+        event.preventDefault();
+        newRecipe.steps.push(step.value);
+        createNewSteps(step.value);
+    }
+  
+
+    addIngridientBtn.addEventListener('click',addNewIngridient);
+    addStepBtn.addEventListener('click', addNewStep);
 
 
 
+    var saveAndCloseBTN = document.querySelector("#new-recipe-btn");
+    var nameRecipe = document.querySelector("#name-recipe");
+    var dirscriptionRecipe = document.querySelector(".discrption-recipe");
+    var listOfStepsRecipe = document.querySelector('#instruction-steps')
+    var listOfIngridients = document.querySelector("#list-of-ingridients");
 
 
+//tworzenie obiektu zawierającego caly przepis
+
+var newRecipe = {
+    name:"",
+    discription:"",
+    steps: [],
+    ingridients:[]
+}
+//zapisywanie przepisu
+
+function saveRecipe(newObj){
+    var dataFromLocalStorage = [];
+    if (localStorage.getItem("newRecipe") != null) {
+        dataFromLocalStorage = JSON.parse(localStorage.getItem("newRecipe"));
+        dataFromLocalStorage.push(newObj);
+        localStorage.setItem("newRecipe", JSON.stringify(dataFromLocalStorage));
+        }else{
+        dataFromLocalStorage.push(newObj);
+        localStorage.setItem("newRecipe", JSON.stringify(dataFromLocalStorage));
+        }
+        alert("Przepis zapisany");
+}
+
+    function saveRecipeLocalStorage(event){
+        event.preventDefault();
+        newRecipe.name = nameRecipe.value;
+        newRecipe.discription = dirscriptionRecipe.value;
+        saveRecipe(newRecipe);
+        screen.style.display = "block";
+        addRecipesPage.style.display = "none";
+
+    }
+
+    saveAndCloseBTN.addEventListener('click', saveRecipeLocalStorage);
 
 
 
 
 })
+
